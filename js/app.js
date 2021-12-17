a400ace391b8792e934389d40b3033b4aedd25cc
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2021-12-17 17:08:51 +0000

    contact info

--- js/app.js
+++ js/app.js
@@ -10,7 +10,13 @@ const offcanvasBottom = document.querySelector('.bottom-offcanvas') // bottom of
 const bottomHeader = document.querySelector('.bottom-header') //bottom offcanvas header
 const navItems = document.querySelectorAll('.accordion-header') // nav item
 const accordionButtons =document.querySelectorAll('a.nav-link.accordion-button') // accordian button
+const accordianBody = document.querySelectorAll('.accordion-body')
 const heroSection = document.querySelector('.hero') //hero section
+const cardBody = document.querySelectorAll('.card-body') // Project card body
+const cardimages = document.querySelectorAll('.card-img-top')
+const bio = document.querySelector('.brif-bio')
+const scrollingP = document.querySelector('.scrolling-p')
+const btnCloseBtm =document.querySelector('.btn-bottom')
 
 const handleMode = () => {
     if(slider.checked){
@@ -18,28 +24,44 @@ const handleMode = () => {
         navbar.classList.add('bg-light') //nav-bar background
         navbutton.classList.add('light-border') // nav button 
         brandName.classList.add('dark-text') //navbar brand color
-        offcanvasNav.classList.add('light-background') //offcanvas menue
+        offcanvasNav.classList.add('bg-light') //offcanvas menue
         offcanvasBottom.classList.add('light-background') //bottom offcanvas
 
         navItems.forEach(item => {// Each nav item
-            item.style.backgroundColor = '#fff'
-            item.style.borderColor = 'black'
+            item.style.backgroundColor = 'transparent'
+            item.style.borderColor = "#000"
         })
 
         accordionButtons.forEach(button => { // Each accordian button
-            button.style.backgroundColor = '#fff'
+            button.style.backgroundColor = 'transparent'
         })
 
         heroSection.style.backgroundImage = 'none' //hero section
 
-        bottomHeader.style.backgroundColor = 'rgb(97, 192, 255)' //bottom offcanvas header
+        bottomHeader.classList.add('light-background') //bottom offcanvas header
+
+        cardBody.forEach(item =>{
+            item.style.backgroundColor = '#fff'
+            item.style.color = '#000'
+        })
+
+        cardimages.forEach((card, index) =>{
+            card.src = `/img/project-${index + 1}-lightmode.jpg`
+        })
+
+        bio.classList.add('dark-text')
+
+        scrollingP.classList.add('dark-text');
+        scrollingP.style.border = "none"
+
+        btnCloseBtm.classList.remove('btn-close-white')
 
     }else if(!slider.checked){
         body.classList.remove('light-background') // body background
         navbar.classList.remove('bg-light') // naav-bar background
         navbutton.classList.remove('light-border')
         brandName.classList.remove('dark-text') //navbar brand color
-        offcanvasNav.classList.remove('light-background') //offcanvas menue
+        offcanvasNav.classList.remove('bg-light') //offcanvas menue
         offcanvasBottom.classList.remove('light-background') //bottom offcanvas
 
         accordionButtons.forEach(button => { //Each accordian button
@@ -55,5 +77,20 @@ const handleMode = () => {
 
 
         bottomHeader.style.backgroundColor = 'black' //bottom offcanvas header
+
+        cardBody.forEach(item =>{
+            item.style.backgroundColor = '#000'
+            item.style.color = '#fff'
+        })
+
+        cardimages.forEach((card, index) =>{
+            card.src = `/img/project-${index + 1}-darkmode.jpg`
+        })
+
+        bio.classList.remove('dark-text')
+
+        btnCloseBtm.classList.add('btn-close-white');
+
+
     }
 }
